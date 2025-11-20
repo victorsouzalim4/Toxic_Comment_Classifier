e8b99e9a4d2ef2769476c231a610a1354a83753d
Victor <victorsouzalima2064@gmail.com>
2025-11-19 22:17:46 -0300

    feat: remoçao de ruidos, normalização de contrações e tokenização por palavra implementada

--- treat_data.py
+++ treat_data.py
@@ -1,19 +1,61 @@
 import unicodedata
 import re
 
+CONTRACTIONS = {
+    r"\bcan't\b": "cannot",
+    r"\bwon't\b": "will not",
+    r"\bdon't\b": "do not",
+    r"\bdoesn't\b": "does not",
+    r"\bdidn't\b": "did not",
+    r"\bi'm\b": "i am",
+    r"\bit's\b": "it is",
+    r"\bhe's\b": "he is",
+    r"\bshe's\b": "she is",
+    r"\byou're\b": "you are",
+    r"\bthey're\b": "they are",
+    r"\bwe're\b": "we are",
+    r"\blet's\b": "let us",
+    r"\bcouldn't\b": "could not",
+    r"\bshouldn't\b": "should not",
+    r"\bwouldn't\b": "would not",
+}
+
 def treat_comment(comment: str):
     # print(comment)
+    comment = comment.lower()
+    comment = normalize_contractions(comment)
+    comment = remove_noise(comment)
     comment = normalize_text(comment)
-    return comment
+    tokens = comment.split()
+    return tokens
 
 def normalize_text(comment: str):
-    comment = comment.lower()
+    
     comment = ''.join(
         c for c in unicodedata.normalize('NFKD', comment)
         if not unicodedata.combining(c)
     )
 
-    comment = re.sub(r'\s+', ' ', comment).strip()
-    comment = re.sub(r'[^a-zA-Z0-9 ]+', '', comment)
+    comment = re.sub(r'[^a-zA-Z0-9 ]+', ' ', comment) # remove caracteres especiais
+    comment = re.sub(r'\s+', ' ', comment).strip()  # remove sequencia de espaços
+ 
+    return comment
 
+def remove_noise(comment: str):     
+    comment = re.sub(r'http[s]?://\S+', ' ', comment)
+    comment = re.sub(r'www\.\S+', ' ', comment)
+    
+    # 3) remover tags HTML
+    comment = re.sub(r'<[^>]+>', ' ', comment)
+    
+    # 4) remover menções e hashtags (@user, #tag)
+    comment = re.sub(r'[@#][^\s]+', ' ', comment)
+
+    return comment
+
+
+def normalize_contractions(comment):
+
+    for pattern, replacement in CONTRACTIONS.items():
+        comment = re.sub(pattern, replacement, comment)
     return comment
